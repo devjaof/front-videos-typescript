a9694d2ee2944ac465352463add36b77a319cb1b
joaofranciscor <joao.rodrigues@viasoft.com.br>
2023-01-03 09:40:45 -0300

    feat: cria o tema e adiciona rotas teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Typography } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/system';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import * as React from 'react';
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+
+const Home = () => {
+  return(
+    <Box>
+      <Typography variant="h3" component="h3">
+        Home
+      </Typography>
+    </Box>
+  )
+}
+
+const About = () => {
+  return (
+    <Box>
+    <Typography variant="h3" component="h3">
+      About
+    </Typography>
+  </Box>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={appTheme}>
+      <Box
+        component="main"
+        sx={{
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "#212121"
+        }}
+      >
+        <Header/>
+        <Layout>
+          <h1>WELCOME</h1>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </Layout>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App;
